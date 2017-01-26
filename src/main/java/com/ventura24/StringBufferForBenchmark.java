95b3ea4df2174ad1db790a5d220c9f5688ac4abb
evalb <esther.valbuena@ventura24.es>
2017-01-26 18:00:10 +0100

    Testing JMH

---
+++ src/main/java/com/ventura24/StringBufferForBenchmark.java
@@ -0,0 +1,38 @@
+package com.ventura24;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+
+public class StringBufferForBenchmark {
+
+    @State(Scope.Benchmark)
+    public static class MyCounter {
+        public int count = 0 ;
+    }
+
+    @Benchmark
+    public void tesForStringBuffer(MyCounter state) {
+        state.count++;
+        StringBuffer stringBuffer = new StringBuffer(state.count);
+        for (int i = 0; i< state.count; i++){
+            stringBuffer.append("join ... strings ");
+            stringBuffer.append(i);
+            stringBuffer.append("of");
+            stringBuffer.append(state.count);
+        }
+        //System.out.println(stringBuffer.toString());
+    }
+
+    @Benchmark
+    public void testForStringAdd(MyCounter state) {
+        state.count++;
+        String stringAdd = "" + state.count;
+        for (int i = 0; i< state.count; i++) {
+            stringAdd = stringAdd + "join ... strings " + state.count;
+        }
+        //System.out.println(stringAdd);
+    }
+
+
+}
